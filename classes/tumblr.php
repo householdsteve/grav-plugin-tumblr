526ed20f125949a8cc5e9a4ca3c73ee004f5a073
stevepofd <steve@pofd.com>
2015-12-10 00:08:58 +0100

    cleaned up some code. needs all possible content types

--- classes/tumblr.php
+++ classes/tumblr.php
@@ -14,19 +14,11 @@ class Tumblr extends Iterator {
     public function __construct(Grav $grav, $page) {
         require_once __DIR__ . '/../vendor/autoload.php';
         
-        /*$consumerKey = $grav['config']->get('plugins.tumblr.settings.consumerKey');
-        $consumerSecret = $grav['config']->get('plugins.tumblr.settings.consumerSecret');*/
-        //echo '<pre>'; print_r($page->header()->tumblr['settings']['page']); echo '</pre>';
+
         $consumerKey = $page->header()->tumblr['settings']['consumerKey'];
         $consumerSecret = $page->header()->tumblr['settings']['consumerSecret'];
           
         $this->client  = new \Tumblr\API\Client($consumerKey, $consumerSecret);
         
-        /* ----
-        this comes from the tumblr indications it probably only needs to be managed to make modifications to content
-        //$this->client ->setToken($token, $tokenSecret);
-        ---- */
-        
-
     }
 }
